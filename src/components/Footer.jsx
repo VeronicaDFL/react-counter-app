a0fde7607950a8ced40d5b5d83335bc200844958
Veronica De Felice <verodefelic@gmail.com>
2023-04-13 16:05:03 -0400

    FIRST commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer ()   {
+const githubURL = "https://github.com/veronicadfl/react-counter-app"
+    return(
+        <footer>
+            <p><small>&copy; 2023</small><br />
+            <a href="{githubURL}" target= "_blank" rel="noreferrer">See code in Github</a></p>
+
+
+        </footer>
+    )
+}
